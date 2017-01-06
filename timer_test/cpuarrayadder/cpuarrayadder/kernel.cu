a99449bc40f1c2e97001cc946712d51602494e41
Rob Holan <silversquirrel88@gmail.com>
2017-01-06 12:03:46 -0600

    Unlimited Comment Works
    TODO: have less commented-out code, maybe remove some of the random VS
    files

--- timer_test/cpuarrayadder/cpuarrayadder/kernel.cu
+++ timer_test/cpuarrayadder/cpuarrayadder/kernel.cu
@@ -1,23 +1,29 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include "../../high_performance_timer/highperformancetimer.h"
 
 #include <stdio.h>
 #include <iostream>
 #include <string>
 #include <stdlib.h>
 #include <time.h>
+//#include <omp.h>
 
 using namespace std;
 
 bool allocateMem(int** a, int** b, int** c, int size);
 void fillArrays(int* a, int* b, int* c, int arrSize);
+void addVector(int* a, int* b, int* c, int arrSize);
 void cleanup(int* a, int* b, int* c);
 
 int main(int argc, char* argv[]) 
 {
 	//seed the rng for filling a and b with random numbers later
 	srand(time(NULL));
+	//set up some variables for timing fillArrays() later
+	double totalTime = 0.0;
+	HighPrecisionTime hpt;
 	//cout << argc << endl;
 	//cout << argv[0] << endl;
 
@@ -41,17 +47,40 @@ int main(int argc, char* argv[])
 	bool success = allocateMem(&a, &b, &c, arraySize);
 
 	//setup tells the user if anything goes horribly wrong, but let's give some good news if it goes well
-	if (success) {
+	if (success) 
+	{
 		cout << "All arrays allocated successfully" << endl;
 		
-		//We want to fill the arrays if we succeeded
-		fillArrays(a, b, c, arraySize);
+		//it's hard to get an accurate time since last call if there is no last call
+		//hpt.TimeSinceLastCall();
+		//fill the arrays, time the process
+		for (int i = 0; i < 100; i++) {
+			//We want to fill the arrays if we succeeded
+			fillArrays(a, b, c, arraySize);
+			//time the process
+			//totalTime += hpt.TimeSinceLastCall();
+			hpt.TimeSinceLastCall();
+			addVector(a, b, c, arraySize);
+			totalTime += hpt.TimeSinceLastCall();
+		}
+		//convert totalTime to the average time (since that's the end goal) and print it
+		totalTime = totalTime / 100;
+		cout << "The average time to add up arrays was " << totalTime << " seconds." << endl;
+
 		//Make sure the arrays were filled
-		for (int i = 0; i < arraySize; i++) {
+		/*for (int i = 0; i < arraySize; i++) {
 			cout << "a[" << i << "] = " << a[i] << endl;
 			cout << "b[" << i << "] = " << b[i] << endl;
 			cout << "c[" << i << "] = " << c[i] << endl;
-		}
+		}*/
+
+		//add the arrays' contents
+		//addVector(a, b, c, arraySize);
+
+		//make sure this worked
+		/*for (int i = 0; i < arraySize; i++) {
+			cout << "c[" << i << "] = " << c[i] << endl;
+		}*/
 	}
 
 	//Free them
@@ -98,12 +127,20 @@ void fillArrays(int* a, int* b, int* c, int arrSize)
 {
 	//fill a and b with small random numbers, fill c with 0
 	//we can use one loop because all arrays are the same size
+//#pragma omp parallel for
 	for (int i = 0; i < arrSize; i++)
 	{
 		//0-19 seems smallish enough
 		a[i] = rand() % 20;
 		b[i] = rand() % 20;
 		c[i] = 0;
+	}
+}
+
+void addVector(int* a, int* b, int* c, int arrSize)
+{
+	for (int i = 0; i < arrSize; i++) {
+		c[i] = a[i] + b[i];
 	}
 }
 
